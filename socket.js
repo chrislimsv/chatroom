a93c22c61d0fe88b2b147f818a8fe813a97b0c6e
Keon Chris Lim <keon.chris.lim@gmail.com>
2014-03-18 00:54:47 -0400

    added # of peeps online

--- socket.js
+++ socket.js
@@ -1,6 +1,6 @@
 var io = require("socket.io");
 var result = "";
-
+var online = 0;
 
 function start(s) {
 	// start the socket
@@ -8,13 +8,26 @@ function start(s) {
 
 	// listen for stuff
 	io.sockets.on('connection', function(socket) {
+		online++;
+
 		socket.emit("message_to_client", {message: result}); 
+		io.sockets.emit('message_online', {message: "There are " + online +
+			" people online!"});
 
 		socket.on('message_to_server', function(data) {
 			result += "\n" + data["message"];
 			io.sockets.emit("message_to_client", {message: result});
 		});
+
+		socket.on('disconnect', function () {
+			online--;
+			io.sockets.emit('message_online', {message: "There are " + online +
+				" people online!"});
+			
+		});
+
 	});
+
 
 }
 
